f165d9be6eb5f954153d563833d7ebf6a6b09431
g-soldera <gustavosevere@gmail.com>
2023-05-20 03:29:16 -0300

    simplify comments

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// "mode" variable to lines 42-49
-// let mode = "dark"
+/* "mode" variable to lines 42-49
+let mode = "dark" */
 
 function toggleMode() {
   // Select <html> element
@@ -16,35 +16,35 @@ function toggleMode() {
     // Change avatar image when light mode
     avatar.setAttribute("src", "assets/avatar.png")
 
-    // Turn grayscale filter (web-safe) on when lightmode
-    // avatar.setAttribute(
-    //   "style",
-    //   "-webkit-filter: grayscale(100%); -webkit-filter: grayscale(1) filter: grayscale(100%);"
-    // )
+    /* Turn grayscale filter (web-safe) on when lightmode
+    avatar.setAttribute(
+      "style",
+      "-webkit-filter: grayscale(100%); -webkit-filter: grayscale(1) filter: grayscale(100%);"
+    ) */
   } else {
     // Change avatar image when dark mode
     avatar.setAttribute("src", "assets/avatar.png")
 
-    // Turn grayscale filter (web-safe) off
-    // avatar.setAttribute(
-    //   "style",
-    //   "-webkit-filter: grayscale(0%); -webkit-filter: grayscale(0) filter: grayscale(0%);"
-    // )
+    /* Turn grayscale filter (web-safe) off
+    avatar.setAttribute(
+      "style",
+      "-webkit-filter: grayscale(0%); -webkit-filter: grayscale(0) filter: grayscale(0%);"
+    ) */
   }
 
-  // Add and remove light mode with classList content
-  // if (html.classList.contains("light")) {
-  //   html.classList.remove("light")
-  // } else {
-  //   html.classList.add("light")
-  // }
+  /* Add and remove light mode with classList content
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  } */
 
-  // Add and remove light mode with cache variable "mode" - Add class to <body> element
-  // if (mode === "light") {
-  //   mode = "dark"
-  //   document.body.classList.remove("light")
-  // } else {
-  //   mode = "light"
-  //   document.body.classList.add("light")
-  // }
+  /* Add and remove light mode with cache variable "mode" - Add class to <body> element - UNCOMMENT VARIABLE TO USE
+  if (mode === "light") {
+    mode = "dark"
+    document.body.classList.remove("light")
+  } else {
+    mode = "light"
+    document.body.classList.add("light")
+  } */
 }
